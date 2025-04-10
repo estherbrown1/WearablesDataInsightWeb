8df6d5ec6893f9b666f9ff27d5d5393b3f1de50c
Karis Moon <karis.moon2023@gmail.com>
2025-04-10 04:01:34 -0400

    Add dotenv to requirements

--- main.py
+++ main.py
@@ -10,72 +10,75 @@ from upload_calendar import calendar_upload_page
 from download_data import download_user_data
 from sql_utils import get_admin_name
 from home import home_page
-from physiological_analysis import run_stepper_extraction 
+from physiological_analysis import run_stepper_extraction
 
 load_dotenv()
 password = os.getenv("PASSWORD")
 with streamlit_analytics.track(unsafe_password=password):
+
     def main():
 
-        st.sidebar.title('Main Menu')
-        page = st.sidebar.radio('Go to', [
-            password,
-            'Home',
-            'Login',
-            'Enter Annotations',
-            'Show Annotations',
-            'Compare Interventions',
-            'Calendar Upload',
-            'Download Data - Admin',
-            'Feature Extractions - Admin',
-            'Upload Data - Admin',
-            'Signup - Admin',
-        ])
+        st.sidebar.title("Main Menu")
+        page = st.sidebar.radio(
+            "Go to",
+            [
+                "Home",
+                "Login",
+                "Enter Annotations",
+                "Show Annotations",
+                "Compare Interventions",
+                "Calendar Upload",
+                "Download Data - Admin",
+                "Feature Extractions - Admin",
+                "Upload Data - Admin",
+                "Signup - Admin",
+            ],
+        )
 
-        st.session_state.logged_in = st.session_state.get('logged_in', False)
+        st.session_state.logged_in = st.session_state.get("logged_in", False)
 
-
-        if page == 'Home':
+        if page == "Home":
             home_page()
-        elif page == 'Signup - Admin':
+        elif page == "Signup - Admin":
             signup_page()
-        elif page == 'Login':
+        elif page == "Login":
             login_successful = login_page()
             if login_successful:
-                st.session_state.page = 'Upload Data'
-        elif page == 'Upload Data - Admin':
+                st.session_state.page = "Upload Data"
+        elif page == "Upload Data - Admin":
             if not st.session_state.logged_in:
                 st.warning("You must log in to access this page.")
                 return
             data_upload()
-        elif page == 'Calendar Upload':
+        elif page == "Calendar Upload":
             if not st.session_state.logged_in:
                 st.warning("You must log in to access this page.")
                 return
             calendar_upload_page()
-        elif page == 'Enter Annotations':
+        elif page == "Enter Annotations":
             if not st.session_state.logged_in:
                 st.warning("You must log in to access this page.")
                 return
             visualization_page(annotation=False)
-        elif page == 'Show Annotations':
+        elif page == "Show Annotations":
             if not st.session_state.logged_in:
                 st.warning("You must log in to access this page.")
                 return
             visualization_page(annotation=True)
-        elif page == 'Compare Interventions':
+        elif page == "Compare Interventions":
             if not st.session_state.logged_in:
                 st.warning("You must log in to access this page.")
                 return
             visualization_page(diff_plot=True)
-        elif page == 'Download Data - Admin':
-            if not (st.session_state.logged_in and get_admin_name() == st.session_state.user):
+        elif page == "Download Data - Admin":
+            if not (
+                st.session_state.logged_in and get_admin_name() == st.session_state.user
+            ):
                 st.warning("You must log in as the admin to access this page.")
                 return
             download_user_data()
-        elif page == 'Feature Extractions - Admin':
-            run_stepper_extraction()  
+        elif page == "Feature Extractions - Admin":
+            run_stepper_extraction()
 
-
-    if __name__ == '__main__':
+    if __name__ == "__main__":
         main()
